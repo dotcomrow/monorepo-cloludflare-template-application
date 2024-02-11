2d4f7a5038873f6b70f4ca0e3ea5af8bdc426727
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-02-11 23:27:11 +0000

    Deploying to release from @ dotcomrow/monorepo-cloludflare-template-application@484849ab0fd430ada45c95445d7763426ed5fb40 🚀

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "google" {
+  region  = "${var.region}"
+}
+
+provider "google-beta" {
+  region  = "${var.region}"
+}
+
+# module "mfe" {
+#   source  = "app.terraform.io/dotcomrow/mfe/cloudflare"
+#   version = "> 1.0.0"
+#   cloudflare_account_id = "${var.cloudflare_account_id}"
+#   cloudflare_zone_id = "${var.cloudflare_zone_id}"
+# }
+
+module "user-preferences-svc" {
+  source  = "./src/services/preferences"
+  cloudflare_account_id = "${var.cloudflare_account_id}"
+  cloudflare_worker_hostname = "user-prefs.${var.domain}"
+  cloudflare_worker_url_pattern = "user-prefs.${var.domain}/*"
+  cloudflare_worker_zone_id = "${var.cloudflare_zone_id}"
+  cloudflare_cors_domains = ".*.${var.domain},.*localhost.*"
+  user_profile_svc_url = "${var.user_profile_svc_endpoint}"
+}

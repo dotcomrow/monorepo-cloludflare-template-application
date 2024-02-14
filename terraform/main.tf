707c717de57e2cff87eb74fb127eb45e15bbab26
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-02-13 21:16:15 -0500

    update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,3 @@
-provider "google" {
-  region  = "${var.region}"
-}
-
-provider "google-beta" {
-  region  = "${var.region}"
-}
 
 # module "mfe" {
 #   source  = "app.terraform.io/dotcomrow/mfe/cloudflare"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,52 +1,3 @@
-variable "region" {
-  description = "The region to deploy resources"
-  type        = string
-}
-
-variable "gcp_org_id" {
-  description = "The GCP organization id"
-  type        = string
-}
-
-variable "billing_account" {
-  description = "The billing account id"
-  type        = string
-}
-
-variable "suffix" {
-  description = "The suffix to append to project names"
-  type        = string
-}
-
-variable "bigquery_secret" {
-  description = "Bigquery secret to use for the service account"
-  type        = string
-  nullable = false
-}
-
-variable "python_session_secret" {
-  description = "Python session secret to use for the service account"
-  type        = string
-  nullable = false
-}
-
-variable "common_project_id" {
-  description = "value of common project id"
-  type        = string
-  nullable = false
-}
-
-variable "audience" {
-  description = "audience for the service account"
-  type        = string
-  nullable = false
-}
-
-variable "config_security_group" {
-  description = "security group for configuration"
-  type        = string
-  nullable = false
-}
 
 variable "domain" {
   description = "domain hosted in cloudflare"
@@ -68,12 +19,6 @@ variable "cloudflare_account_id" {
 
 variable "cloudflare_zone_id" {
   description = "cloudflare worker zone id"
-  type        = string
-  nullable = false
-}
-
-variable "registry_name" {
-  description = "registry name"
   type        = string
   nullable = false
 }

b29b90524db9821fa5313a06914ee83464f015c8
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-02-14 02:25:46 +0000

    Deploying to release from @ dotcomrow/monorepo-cloludflare-template-application@9879d94c7a394b33b9118b70463010b3671244a5 🚀

---
+++ public/home.tf
@@ -0,0 +1,44 @@
+locals {
+  project_name = "template-app"
+}
+
+resource "cloudflare_pages_domain" "app" {
+  account_id   = var.cloudflare_account_id
+  project_name = "${local.project_name}"
+  domain       = "${local.project_name}.suncoast.systems"
+
+  depends_on = [ cloudflare_pages_project.app ]
+}
+
+resource "cloudflare_record" "app" {
+  zone_id = var.cloudflare_zone_id
+  name    = "${local.project_name}"
+  value   = cloudflare_pages_project.app.domains[0]
+  type    = "CNAME"
+  ttl     = 3600
+  allow_overwrite = true
+}
+
+resource "cloudflare_pages_project" "app" {
+  account_id        = var.cloudflare_account_id
+  name              = "${local.project_name}"
+  production_branch = "gh-pages"
+  source {
+    type = "github"
+    config {
+      owner                         = "dotcomrow"
+      repo_name                     = "monorepo-cloludflare-template-application"
+      production_branch             = "gh-pages"
+      pr_comments_enabled           = true
+      deployments_enabled           = true
+      production_deployment_enabled = true
+      preview_deployment_setting = "none" 
+    }
+  }
+
+  deployment_configs {
+    production {
+
+    }
+  }
+}

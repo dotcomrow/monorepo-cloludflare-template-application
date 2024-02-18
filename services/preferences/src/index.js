15957dfdfae8e3f91fd733051bfa32b2c0b3c9d0
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-02-18 17:33:05 -0500

    adding log
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- services/preferences/src/index.js
+++ services/preferences/src/index.js
@@ -4,6 +4,7 @@ import { serializeError } from "serialize-error";
 export default {
   async fetch(request, env, context) {
     try {
+      console.log("handleRequest")
       return await handleRequest(request, env, context);
     } catch (e) {
       const responseError = serializeError(e);
